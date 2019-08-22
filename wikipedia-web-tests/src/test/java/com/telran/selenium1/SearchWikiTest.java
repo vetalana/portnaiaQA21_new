f269ca7ca45ff0efd13a37717969a3817bfe2a6d
vetalana <54117600+vetalana@users.noreply.github.com>
2019-08-22 18:06:51 +0300

    WikiTest

---
+++ wikipedia-web-tests/src/test/java/com/telran/selenium1/SearchWikiTest.java
@@ -0,0 +1,27 @@
+package com.telran.selenium1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SearchWikiTest {
+WebDriver driver;
+@Test
+public void SearchWiki() throws InterruptedException {
+    driver=new ChromeDriver();
+    //open site:
+    driver.get("https://en.wikipedia.org");
+    //type word "Trafford":
+    driver.findElement(By.name("search")).click();
+    driver.findElement(By.name("search")).clear();
+    driver.findElement(By.name("search")).sendKeys("Trafford");
+    driver.findElement(By.name("go")).click();
+    //pause
+    Thread.sleep(5000);
+    //exit
+    driver.quit();
+
+}
+
+}
