3b2a8caa188f5665a2a6e82a2f3feeb9d2e5a0e9
vetalana <54117600+vetalana@users.noreply.github.com>
2019-10-06 22:51:17 +0300

    222

--- wikipedia-web-tests/src/test/java/wikitestsprogressive/tests/LoginWikiTest.java
+++ wikipedia-web-tests/src/test/java/wikitestsprogressive/tests/LoginWikiTest.java
@@ -9,7 +9,7 @@ public class LoginWikiTest extends TestBase {
     @Test
     public void loginTest() {
         app.getSessionHelper().initLogin();
-        app.getSessionHelper().fillLoginForm("svetlana.portnaia@gmail.com", "Aa1234556789");
+        app.getSessionHelper().fillLoginForm("svetlana.portnaia@gmail.com", "Aa123456789");
         app.getSessionHelper().confirmLogin();
     }
 
